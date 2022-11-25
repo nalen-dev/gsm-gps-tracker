024b24553eb314dc69a3ebeaee9bbc2305f927dc
nlndrpraja <nalendrapraja11@gmail.com>
2022-11-25 21:56:46 +0800

    read latitude longitude from gps module

--- gps-gsm-tracker.ino
+++ gps-gsm-tracker.ino
@@ -1,17 +1,24 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
-#include <TimeLib.h>
+
+// initialize pin
+static const int GPSRx = 3, GPSTx = 4;
+float Lat, Lng;
+
+// setup
+SoftwareSerial SerialGPS(GPSRx, GPSTx);
+TinyGPSPlus Gps;
 
 struct data_gps
 {
     float latitude;
     float longitude;
     float temp;
-    char message[256];
+    char message[100];
     void setMessage()
     {
-        char slat[12];
-        char slng[12];
+        char slat[15];
+        char slng[15];
         char stmp[5];
         dtostrf(latitude, 9, 6, slat);
         dtostrf(longitude, 10, 6, slng);
@@ -23,9 +30,30 @@ struct data_gps
 void setup()
 {
     Serial.begin(9600);
+    SerialGPS.begin(9600);
     Serial.println(F("Mulai!!"));
 }
 
 void loop()
 {
+    struct data_gps newData = getDataGps();
+    Serial.println(newData.message);
+}
+
+struct data_gps getDataGps()
+{
+    struct data_gps newGpsData;
+    SerialGPS.listen();
+    while (SerialGPS.available() > 0)
+    {
+        Gps.encode(SerialGPS.read());
+        if (Gps.location.isValid())
+        {
+            newGpsData.latitude = Gps.location.lat();
+            newGpsData.longitude = Gps.location.lng();
+            newGpsData.temp = 30;
+        };
+        newGpsData.setMessage();
+    };
+    return newGpsData;
 }
