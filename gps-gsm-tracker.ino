8e5120f84db33413fe26ebbfc93c511e466e7f90
nlndrpraja <nalendrapraja11@gmail.com>
2022-11-26 00:42:33 +0800

    add ajust time manual

--- gps-gsm-tracker.ino
+++ gps-gsm-tracker.ino
@@ -1,19 +1,22 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+#include <TimeLib.h>
 
 // initialize pin
 static const int GPSRx = 3, GPSTx = 4;
-float Lat, Lng;
 
 // setup
 SoftwareSerial SerialGPS(GPSRx, GPSTx);
 TinyGPSPlus Gps;
+
+String adjustTimeNow(int H, int M);
 
 struct data_gps
 {
     float latitude;
     float longitude;
     float temp;
+    String nowT;
     char message[100];
     void setMessage()
     {
@@ -36,8 +39,10 @@ void setup()
 
 void loop()
 {
+    String a = adjustTimeNow(Gps.time.hour(), Gps.time.minute());
     struct data_gps newData = getDataGps();
     Serial.println(newData.message);
+    Serial.println(a);
 }
 
 struct data_gps getDataGps()
@@ -52,8 +57,22 @@ struct data_gps getDataGps()
             newGpsData.latitude = Gps.location.lat();
             newGpsData.longitude = Gps.location.lng();
             newGpsData.temp = 30;
+            break;
         };
         newGpsData.setMessage();
     };
     return newGpsData;
+}
+
+String adjustTimeNow(int H, int M)
+{
+    char Time[6];
+    setTime(H, M, 00, 01, 12, 2022);
+    int hAdjust = H + 8;
+    if (hAdjust > 23)
+    {
+        hAdjust -= 24;
+    }
+    sprintf(Time, "%d:%d", hAdjust, M);
+    return Time;
 }
