c4106d6d3fb70ca8562cbd0a4ef96ab5e84ec81f
nlndrpraja <nalendrapraja11@gmail.com>
2022-11-26 09:49:22 +0800

    basic read temperature

--- gps-gsm-tracker.ino
+++ gps-gsm-tracker.ino
@@ -1,78 +1,19 @@
-#include <TinyGPS++.h>
-#include <SoftwareSerial.h>
-#include <TimeLib.h>
+#include "DHT.h"
+#define DHTTYPE DHT11
 
-// initialize pin
-static const int GPSRx = 3, GPSTx = 4;
-
-// setup
-SoftwareSerial SerialGPS(GPSRx, GPSTx);
-TinyGPSPlus Gps;
-
-String adjustTimeNow(int H, int M);
-
-struct data_gps
-{
-    float latitude;
-    float longitude;
-    float temp;
-    String nowT;
-    char message[100];
-    void setMessage()
-    {
-        char slat[15];
-        char slng[15];
-        char stmp[5];
-        dtostrf(latitude, 9, 6, slat);
-        dtostrf(longitude, 10, 6, slng);
-        dtostrf(temp, 5, 2, stmp);
-        sprintf(message, "Temp: %s C\nLat: %s\nLong: %s", stmp, slat, slng);
-    }
-};
+DHT dht(5, DHTTYPE);
 
 void setup()
 {
     Serial.begin(9600);
-    SerialGPS.begin(9600);
-    Serial.println(F("Mulai!!"));
+    Serial.println("Pengujian DHT11");
+    dht.begin();
 }
 
 void loop()
 {
-    String a = adjustTimeNow(Gps.time.hour(), Gps.time.minute());
-    struct data_gps newData = getDataGps();
-    Serial.println(newData.message);
-    Serial.println(a);
-}
-
-struct data_gps getDataGps()
-{
-    struct data_gps newGpsData;
-    SerialGPS.listen();
-    while (SerialGPS.available() > 0)
-    {
-        Gps.encode(SerialGPS.read());
-        if (Gps.location.isValid())
-        {
-            newGpsData.latitude = Gps.location.lat();
-            newGpsData.longitude = Gps.location.lng();
-            newGpsData.temp = 30;
-            break;
-        };
-        newGpsData.setMessage();
-    };
-    return newGpsData;
-}
-
-String adjustTimeNow(int H, int M)
-{
-    char Time[6];
-    setTime(H, M, 00, 01, 12, 2022);
-    int hAdjust = H + 8;
-    if (hAdjust > 23)
-    {
-        hAdjust -= 24;
-    }
-    sprintf(Time, "%d:%d", hAdjust, M);
-    return Time;
+    float celcius = dht.readTemperature();
+    Serial.print("Suhu: ");
+    Serial.print(celcius);
+    Serial.println(" C");
 }
