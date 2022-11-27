466a067a2e487efc2e0a1b77fea7cb93db72d539
nlndrpraja <nalendrapraja11@gmail.com>
2022-11-28 06:52:03 +0800

    add basic send sms

--- gps-gsm-tracker.ino
+++ gps-gsm-tracker.ino
@@ -6,16 +6,19 @@
 
 // initialize pin
 DHT dht(5, DHTTYPE);
-static const int GPSRx = 3, GPSTx = 4;
+static const int GPSRx = 3, GPSTx = 4; // Rx Arduino --> Tx GPS && Tx Arduino --> Rx GPS
+static const int GSMRx = 6, GSMTx = 7; // Rx Arduino --> Tx GSM && Tx Arduino --> Rx GSM
 
 // setup
 SoftwareSerial SerialGPS(GPSRx, GPSTx);
+SoftwareSerial SerialGSM(GSMRx, GSMTx);
+
 TinyGPSPlus Gps;
 
 void adjustTimeNow(int H, int M);
 
 // declare
-char message[50];
+char message[50] = "Hello world!";
 float latitude, longitude, temp;
 char Time[6];
 char stmp[7];
@@ -25,25 +28,31 @@ const unsigned long intervalGSMSend = 60000;
 unsigned long previousTimeGSM = 0;
 unsigned long previousTimeGPS = 0;
 
+bool send = true;
+
 void setup()
 {
     Serial.begin(9600);
     SerialGPS.begin(9600);
+    SerialGSM.begin(9600);
     Serial.println(F("Mulai!!"));
-    dht.begin();
 }
 
 void loop()
 {
+
     unsigned long currentTime = millis();
-    getDataGps();
-    if (currentTime - previousTimeGSM >= intervalGSMSend)
+
+    // getDataGps();
+    if (currentTime - previousTimeGSM >= intervalGSMSend && send == true)
     {
-        temp = dht.readTemperature();
-        dtostrf(temp, 5, 2, stmp);
-        setMessage(latitude, longitude);
-        Serial.println(message);
-        previousTimeGSM = currentTime;
+        // temp = dht.readTemperature();
+        // dtostrf(temp, 5, 2, stmp);
+        // setMessage(latitude, longitude);
+        // Serial.println(message);
+        // previousTimeGSM = currentTime;
+        sendMessage();
+        send = false;
     }
 }
 
@@ -83,4 +92,20 @@ void setMessage(float latitude, float longitude)
     dtostrf(longitude, 11, 6, slng);
     sprintf(message, "Time: %s\nTemp: %s C\nLat: %s\nLong: %s", Time, stmp, slat, slng);
     return;
+}
+
+void sendMessage()
+{
+    Serial.println(F("Sending message ..."));
+    SerialGSM.listen();
+    delay(5000);
+    SerialGSM.print("\r");
+    delay(1000);
+    SerialGSM.print("AT+CMGF=1\r");
+    delay(1000);
+    SerialGSM.print("AT+CMGS=\"082113910596\"\r"); // No HP Tujuan
+    delay(1000);
+    SerialGSM.print(message);
+    delay(5000);
+    SerialGSM.write(0x1A);
 }
